4ecd1f9c2223de791f3973356b8cef61d05653bc
Ji <hope.zh@gmail.com>
2020-04-25 21:30:37 +0800

    test dash

---
+++ test_dash.py
@@ -0,0 +1,45 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+ext_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(
+    __name__, 
+    external_stylesheets = ext_stylesheets
+)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello World'),
+    html.Div(children=
+        '''
+        Dash, A web application framework for Python
+        '''), 
+    dcc.Graph(
+        id='example_graph', 
+        figure={
+            'data' : [
+                        {'x' : [1, 2, 3],
+                         'y' : [4, 1, 2], 
+                         'type' : 'bar',
+                         'name' : 'SF'}, 
+                        {'x' : [1, 2, 3], 
+                         'y' : [2, 4, 5], 
+                         'type' : 'bar', 
+                         'name' : u'Montreal'}, 
+                    ], 
+            'layout' : {'title' : 'Dash Data Visualization'}
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+# %%
+
+
