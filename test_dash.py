0f2b67cadf0cf402495bb54f1e3459d7f6a2d29b
Ji <hope.zh@gmail.com>
2020-04-25 22:07:57 +0800

    test dash

--- test_dash.py
+++ test_dash.py
@@ -1,37 +1,44 @@
+import plotly.express as px
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
 
-ext_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+tips = px.data.tips()
+col_options = [dict(label=x, value=x) for x in tips.columns]
+dimensions = ["x", "y", "color", "facet_col", "facet_row"]
 
 app = dash.Dash(
-    __name__, 
-    external_stylesheets = ext_stylesheets
+    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+)
+server = app.server
+
+app.layout = html.Div(
+    [
+        html.H1("Demo: Plotly Express in Dash with Tips Dataset"),
+        html.Div(
+            [
+                html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
+                for d in dimensions
+            ],
+            style={"width": "25%", "float": "left"},
+        ),
+        dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"}),
+    ]
 )
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Doh...'),
-    html.Div(children=
-        '''
-        Dash, A web application framework for Python
-        '''), 
-    dcc.Graph(
-        id='example_graph', 
-        figure={
-            'data' : [
-                        {'x' : [1, 1, 3],
-                         'y' : [4, 1, 9], 
-                         'type' : 'bar',
-                         'name' : 'SF'}, 
-                        {'x' : [1, 5, 3], 
-                         'y' : [8, 4, 5], 
-                         'type' : 'bar', 
-                         'name' : u'Montreal'}, 
-                    ], 
-            'layout' : {'title' : 'Dash Data Visualization'}
-        }
-    )
-])
 
-if __name__ == '__main__':
+@app.callback(Output("graph", "figure"), [Input(d, "value") for d in dimensions])
+def make_figure(x, y, color, facet_col, facet_row):
+    return px.scatter(
+        tips,
+        x=x,
+        y=y,
+        color=color,
+        facet_col=facet_col,
+        facet_row=facet_row,
+        height=700,
+    )
+
+if __name__ == "__main__":
     app.run_server(debug=True)
